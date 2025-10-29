3ee599726d27d790e580dc3d5b487f2fa87e8c51
m-harrison2023 <m_harrison2023@coloradocollege.edu>
2025-10-29 11:34:54 -0600

    Some of the additional data

---
+++ practical_4.py
@@ -0,0 +1,17 @@
+import import_code as ic
+file_path="/Users/morganharrison/Downloads/ev228_data/"
+file_name= 'AYW00090001_temp_195702-202508.csv'
+column='M-A-M'
+ic.fun_import_code(file_path, file_name, column)
+
+import import_code as ic
+file_path="/Users/morganharrison/Downloads/ev228_data/"
+file_name= 'BR038014410_temp_189601-202508.csv'
+column='D-J-F'
+ic.fun_import_code(file_path, file_name, column)
+
+import import_code as ic
+file_path="/Users/morganharrison/Downloads/ev228_data/"
+file_name= 'IN020100400_temp_189101-202508.csv'
+column='J-J-A'
+ic.fun_import_code(file_path, file_name, column)

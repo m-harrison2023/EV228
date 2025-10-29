600627c1ed98fda2255dccbdc40d4a33a2193bca
m-harrison2023 <m_harrison2023@coloradocollege.edu>
2025-10-29 10:58:14 -0600

    Wrote import code

--- import_code.py
+++ import_code.py
@@ -1,7 +1,16 @@
 import pandas as pn
 import numpy as np
 
-file_path= '/Users/morganharrison/Downloads/ev228_data/'
-file_name= '/KRDU_temp_188708-202508.csv'
-df = pn.read_csv('/KRDU_temp_188708-202508.csv')
-print(df)
+file_path="/Users/morganharrison/Downloads/ev228_data/"
+file_name= 'KRDU_temp_188708-202508.csv'
+df = pn.read_csv(file_path + file_name)
+
+#months_w= df['D-J-F']
+#print(months_w, df.iloc[17])
+
+def fun_import_code(file_path, file_name, column):
+    df= pn.read_csv(file_path + file_name)
+    variable_column= df[column]
+    print(variable_column)
+
+fun_import_code(file_path, file_name, 'D-J-F')
---
+++ wrap_import_code.py
@@ -0,0 +1,5 @@
+import import_code as ic
+file_path="/Users/morganharrison/Downloads/ev228_data/"
+file_name= 'KRDU_temp_188708-202508.csv'
+column='D-J-F'
+ic.fun_import_code(file_path, file_name, column)

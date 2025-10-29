b940f1e79323ac9503ca6a49bd76c322384075ba
m-harrison2023 <m_harrison2023@coloradocollege.edu>
2025-10-29 09:15:03 -0600

    Start of class import_code

--- import_code.py
+++ import_code.py
@@ -1,4 +1,7 @@
-file_path= "/Users/morganharrison/Downloads/ev228_data/"
+import pandas as pn
+import numpy as np
+
+file_path= '/Users/morganharrison/Downloads/ev228_data/'
 file_name= '/KRDU_temp_188708-202508.csv'
-df = pandas.read_csv('/KRDU_temp_188708-202508.csv')
+df = pn.read_csv('/KRDU_temp_188708-202508.csv')
 print(df)

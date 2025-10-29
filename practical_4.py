5b2875a96c517049581e576e4b4716e8735a1cf3
m-harrison2023 <m_harrison2023@coloradocollege.edu>
2025-10-29 11:40:40 -0600

    Update practical_4.py

--- practical_4.py
+++ practical_4.py
@@ -15,3 +15,33 @@ file_path="/Users/morganharrison/Downloads/ev228_data/"
 file_name= 'IN020100400_temp_189101-202508.csv'
 column='J-J-A'
 ic.fun_import_code(file_path, file_name, column)
+
+import import_code as ic
+file_path="/Users/morganharrison/Downloads/ev228_data/"
+file_name= 'KSM00047108_temp_190710-202508.csv'
+column='M-A-M'
+ic.fun_import_code(file_path, file_name, column)
+
+import import_code as ic
+file_path="/Users/morganharrison/Downloads/ev228_data/"
+file_name= 'ROE00108901_temp_188001-202508.csv'
+column='J-J-A'
+ic.fun_import_code(file_path, file_name, column)
+
+import import_code as ic
+file_path="/Users/morganharrison/Downloads/ev228_data/"
+file_name= 'RSM00021432_temp_193601-202508.csv'
+column='J-J-A'
+ic.fun_import_code(file_path, file_name, column)
+
+import import_code as ic
+file_path="/Users/morganharrison/Downloads/ev228_data/"
+file_name= 'SG000061641_temp_189906-202508.csv'
+column='J-J-A'
+ic.fun_import_code(file_path, file_name, column)
+
+import import_code as ic
+file_path="/Users/morganharrison/Downloads/ev228_data/"
+file_name= 'USW00093009_temp_190801-202508.csv'
+column='J-J-A'
+ic.fun_import_code(file_path, file_name, column)

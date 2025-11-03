a470a8e8d8d8d2e5926fc8d03f58c8d3c9df4f53
m-harrison2023 <m_harrison2023@coloradocollege.edu>
2025-11-02 21:07:43 -0700

    Individual Project
    Playing around with one set of data, may change later, hoping to see formatting of plot to inform final data set.

--- individual_project.py
+++ individual_project.py
@@ -3,7 +3,15 @@ import pandas as pn
 import xarray as xr
 import netCDF4 as nc
 
+#2005 Convective Precipitation and High Vegetation Cover
+#fn="/Users/morganharrison/Downloads/ev228_data/"
+#fp= 'data_stream-moda_stepType-avgad.nc'
+#ds = nc.Dataset(fn+fp)
+#print(ds)
+
 fn="/Users/morganharrison/Downloads/ev228_data/"
-fp= 'precip+vegetation.zip'
-ds = nc.Dataset(fn+fp)
-print(ds)
+fp= 'data_stream-moda_stepType-avgad.nc'
+ds = xr.open_dataset(fn+fp)
+
+plt.title('Veggetation')
+plt.show()

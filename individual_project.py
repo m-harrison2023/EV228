31f6f7742720daa902ca8fd1378a58c468b14eea
m-harrison2023 <m_harrison2023@coloradocollege.edu>
2025-11-06 13:04:27 -0700

    Trying to see variables and print

--- individual_project.py
+++ individual_project.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as pn
 import pandas as pn
-import xarray as xr
-import netCDF4 as nc
+import numpy as np
 
-#2005 Convective Precipitation and High Vegetation Cover
-fn="/Users/morganharrison/Downloads/ev228_data/"
-fp= 'data_stream-moda_stepType-avgad.nc'
-out_fn= ''
+import fun_plots as fp
+
+'''2005 Convective Precipitation and High Vegetation Cover'''
+fn="/Users/morganharrison/Downloads/ev228_data/vegetation_precip_era5/"
+f_p= '2_data_stream-moda_stepType-avgad.nc'
+out_fn= '/Users/morganharrison/Downloads/ev228_data/graphs/'
 out_fp= '1_indvP.png'
-ds = xr.open_dataset(fn+fp)
-ds.plot()
-plt.title('Veggetation')
-plt.show()
+
+fig_name= 'latitude'
+da_veg = fi.import_era5(file_path= fn+f_p, var=fig_name)
+print(da_veg)
+
+#da_veg_time= da_veg.mean(dim='valid_time')
+#fp.map(da_veg_time, out_path=out_fp, out_name=out_fn)

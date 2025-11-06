cac7e7c48442f2252939b37b4d93a6ed3a14774e
m-harrison2023 <m_harrison2023@coloradocollege.edu>
2025-11-06 13:54:32 -0700

    In class editing code individual

---
+++ fun_gridded.py
@@ -0,0 +1,27 @@
+'''Taken from ev228 Code from Daniel H'''
+import sys
+import matplotlib.pyplot as plt
+import pandas as pd
+import xarray as xr
+
+def map(in_da, out_path='', out_name=''):
+    ''' Plot map from 2D DataArray '''
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    lons = in_da.longitude
+    lats = in_da.latitude
+
+    image = plt.pcolormesh(lons, lats, in_da)
+    plt.xlabel('longitude')
+    plt.ylabel('latitude')
+    plt.title('ERA5 Vegetation')
+    cb = plt.colorbar(image, shrink=.75, orientation="vertical", pad=.02)
+    cb.set_label('Unit')
+    plt.savefig(out_path + out_name, dpi=400)
+
+def import_era5(file_path='', var=''):
+    ''' Import ERA5 gridded data '''
+    ds = xr.open_dataset(file_path)
+    da = ds[var]
+
+    return da
--- individual_project.py
+++ individual_project.py
@@ -1,18 +1,16 @@
 import matplotlib.pyplot as pn
-import pandas as pn
 import numpy as np
+import xarray as xr
 
-import fun_plots as fp
+import fun_gridded as fg
 
 '''2005 Convective Precipitation and High Vegetation Cover'''
-fn="/Users/morganharrison/Downloads/ev228_data/vegetation_precip_era5/"
-f_p= '2_data_stream-moda_stepType-avgad.nc'
-out_fn= '/Users/morganharrison/Downloads/ev228_data/graphs/'
-out_fp= '1_indvP.png'
+f_p="/Users/morganharrison/Downloads/ev228_data/vegetation_precip_era5/"
+f_n= '2_data_stream-moda_stepType-avgad.nc'
+out_path= '/Users/morganharrison/Downloads/ev228_data/graphs/'
+out_fn= '1_indvP.png'
 
-fig_name= 'latitude'
-da_veg = fi.import_era5(file_path= fn+f_p, var=fig_name)
-print(da_veg)
-
-#da_veg_time= da_veg.mean(dim='valid_time')
-#fp.map(da_veg_time, out_path=out_fp, out_name=out_fn)
+variable= 'tp'
+da = fg.import_era5(file_path=f_p + f_n, var=variable)
+da_timemn= da.mean(dim='valid_time')
+fg.map(da_timemn, out_path=out_path, out_name=out_fn)
